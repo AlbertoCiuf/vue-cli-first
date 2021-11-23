d2561dfecb4c28a719e63bf91ba7d77a3e27f23a
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2021-11-23 15:57:23 +0100

    hello world fatto

---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hello World! I'm using Vue CLI and I'm a component!</h1>
+</template>
+
+<script>
+export default {
+  name: "Main"
+}
+</script>
+
+<style scoped>
+  h1 {
+    color: teal;
+    text-shadow: 1px 1px 1px black;
+  }
+</style>
